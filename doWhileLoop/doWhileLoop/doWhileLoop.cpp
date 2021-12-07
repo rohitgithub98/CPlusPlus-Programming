f515101471eaf554205a614789907ce2bdabed9a
rohitgithub98 <tarakarohit@gmail.com>
2021-12-07 07:00:10 +0530

    Added while, doWhile, for loop programs to print n numbers

---
+++ doWhileLoop/doWhileLoop/doWhileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=0, n;
+    cout << "Enter n : ";
+    cin >> n;
+    do
+    {
+        cout << i<<endl;
+        i++;
+    } while (i < 10);
+}
+
